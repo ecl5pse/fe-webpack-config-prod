208b105cd35fbcee1a3954f0be91642f58401d20
ecl5pse <fantamas20@gmail.com>
2019-12-31 16:11:58 +0200

    update webpack config and  update site scss page

--- webpack_config/webpack.config.dev.js
+++ webpack_config/webpack.config.dev.js
@@ -1,37 +1,14 @@
 const path = require( 'path' );
 const merge = require( 'webpack-merge' );
 const commonConfig = require( './webpack.config.common.js' );
-const BundleAnalyzerPlugin = require( 'webpack-bundle-analyzer' ).BundleAnalyzerPlugin;
 
 const developmentConfig = merge( commonConfig, {
   mode: 'development',
-  devtool: 'eval',
+  devtool: 'eval-source-map',
   devServer: {
-    contentBase: path.join( __dirname, '../build' ),
+    contentBase: path.join(__dirname, '../build'),
   },
 
-  module: {
-    rules: [
-      /*
-       * STYLES RULE
-       * */
-      {
-        test: /\.(c|sa|sc)ss$/,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              url: true,
-              sourceMap: true,
-              importLoaders: 2,
-            }
-          },
-          'sass-loader',
-        ]
-      }
-    ]
-  }
-} );
+});
 
 module.exports = developmentConfig;
