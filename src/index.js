153a3ee82ef444eb29cfccd002ce49964628f455
ecl5pse <fantamas20@gmail.com>
2019-12-31 16:40:55 +0200

    add file json and test work json

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-import styles from './assets/sass/style.scss';
+import './assets/sass/style.scss';
+import './assets/sass/reset.scss';
+
+fetch("./data/employees.json")
+.then(response => response.json())
+.then(console.log)
+.catch(console.error)
+
